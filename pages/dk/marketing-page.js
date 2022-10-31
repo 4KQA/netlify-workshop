3eb1b8d0243fe251fcf911ad1882bf0d7277589b
Nicolai Nordstrøm Nielsen <Nicolai.Nielsen@akqa.com>
2022-10-31 16:34:27 +0100

    Fixing dk locale

--- pages/dk/marketing-page.js
+++ pages/dk/marketing-page.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import { Template } from "../../components/Template/Template";
-import styles from "../styles/Home.module.css";
-import background from "../public/images/dk-bg.jpeg";
+import styles from "../../styles/Home.module.css";
+import background from "../../public/images/dk-bg.jpeg";
 import Hero from "../../components/Hero/Hero";
+import { Template } from "../../components/Template/Template";
 
 export default function Home({products, country}) {
   const [hydrated, setHydrated] = React.useState(false);
