9a59d8de8d180fabeb12040ff712c485cebf35e6
bigslott <bigslott@gmail.com>
2022-10-31 15:16:38 +0100

    Use weather info

--- products.js
+++ products.js
@@ -4,41 +4,47 @@ export const products = [
       image:
         "https://pwa-frontend.sport24.dk/_next/image?url=https://img.sport24.dk/catalog/product/3/b/3b-cu9157_010_01.jpg&w=256&q=75",
       price: "Kr. 700,00",
-      season: "WINTER",
+      minTemp: -20,
+      maxTemp: 15
     },
     {
       name: "Flyverdragt",
       image:
         "https://pwa-frontend.sport24.dk/_next/image?url=https://img.sport24.dk/catalog/product/4/1/414-z213005_4241_01.jpg&w=256&q=75",
       price: "Kr. 500,00",
-      season: "WINTER",
+      minTemp: -10,
+      maxTemp: 10
     },
     {
       name: "Handsker",
       image:
         "https://pwa-frontend.sport24.dk/_next/image?url=https://img.sport24.dk/catalog/product/3/4/343-611198_royal_01.jpg&w=256&q=75",
       price: "Kr. 300,00",
-      season: "WINTER",
+      minTemp: -10,
+      maxTemp: 10
     },
     {
       name: "Badebukser",
       image:
         "https://images.asos-media.com/products/tommy-hilfiger-badeshorts-med-tropisk-print-i-polyester-del-af-st/202752577-1-black?$n_320w$&wid=317&fit=constrain",
       price: "Kr. 800,00",
-      season: "SUMMER",
+      minTemp: 20,
+      maxTemp: 40
     },
     {
       name: "Sandler",
       image:
         "https://images.asos-media.com/products/bershka-sorte-sandaler-med-stropper/203228595-1-black?$n_320w$&wid=317&fit=constrain",
       price: "Kr. 299,00",
-      season: "SUMMER",
+      minTemp: 20,
+      maxTemp: 40
     },
     {
       name: "Bæltetaske",
       image:
         "https://images.asos-media.com/products/bershka-sort-crossbody-dobbelttaske-i-nylon/203567709-1-black?$n_320w$&wid=317&fit=constrain",
       price: "Kr. 179,00",
-      season: "SUMMER",
+      minTemp: 10,
+      maxTemp: 40
     },
   ];
