6458ceb983e6dd66d10cbd38d58208060a0637a9
Søren Beier Husted <soren.husted@akqa.com>
2022-10-31 10:54:30 +0100

    Avatar h1

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Personalization</h1>
+      <h1>Avatar</h1>
       {/* 
       <main className={styles.main}>
         <h1 className={styles.title}>
